5a99397ebefd44b5d5ddba2eb5ec2966a179b6d4
Bekjon Ibragimov <bekjon_98@mail.ru>
2024-01-15 20:59:19 +0200

    update

--- src/app/api/questions/[id]/route.js
+++ src/app/api/questions/[id]/route.js
@@ -1,12 +1,13 @@
 
-import { sql } from "@vercel/postgres";
+import prisma from '@/lib/prisma'
 
 
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 export async function GET(request, { params }) {
-    const { rows } = await sql `SELECT * FROM algorithm WHERE uid = ${params.id} LIMIT 1`
-    if (rows) return Response.json(rows[0])
-    return Response.json([])
+    const question = await prisma.question.findFirst({
+        where: { uid: params.id }
+    })
+    return Response.json(question)
 }
 
--- src/app/api/questions/route.js
+++ src/app/api/questions/route.js
@@ -1,18 +1,17 @@
-
+import prisma from '@/lib/prisma'
 import { sql } from "@vercel/postgres";
 
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
 export async function GET(request, { params }) {
-    const { rows } = await sql `SELECT * FROM algorithm`
-    return Response.json(rows);
+    const questions = await prisma.question.findMany();
+    return Response.json(questions);
 }
 
 export async function PUT(request, { params }) {
     try {
         const { uid, scratchpad } = await request.json()
-        console.log(uid, scratchpad)
         await sql`UPDATE algorithm SET scratchpad = ${scratchpad} WHERE uid = ${uid}`
         return Response.json([]);
     }
