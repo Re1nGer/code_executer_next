59b58208aff5d0920dba037b4980e27a40c52d4d
Bekjon Ibragimov <bekjon_98@mail.ru>
2024-01-18 20:22:51 +0200

    content menu added

--- src/components/ContentMenu.jsx
+++ src/components/ContentMenu.jsx
@@ -1,24 +1,95 @@
 import { AnimatePresence } from "framer-motion";
 import InterviewQuestionsIcon from '../icons/InterviewQuestionsIcon.svg'
 import Link from "next/link";
+import ArrowIcon from '../icons/ArrowDown.svg';
+import { motion } from "framer-motion";
+import { useState } from "react";
 
-const ContentMenu = () => {
+const ContentMenu = ({ isOpen }) => {
+
+    //TODO: set up some enum for it
+    const [itemSelected, setItemSelected] = useState('algoexpert');
+
+    const getSelectedItem = (item) => {
+        switch (item) {
+            case 'algoexpert':
+                return <AlgoExpertMenu />
+            case 'systemsexpert':
+                return <SystemExpertMenu />
+            case 'mlexpert':
+                return <MlExpertMenu />
+            case 'infraexpert':
+                return <InfraExpertMenu />
+            case 'blockchainexpert':
+                return <BlockChainExpertMenu />
+            case 'bonus':
+                return <BonusMenu />
+            default:
+                return <AlgoExpertMenu />
+        }
+    }
+
     return (
         <AnimatePresence>
-            <div className={''}></div>
+            { isOpen ? (
+                <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }} exit={{ scale: 0 }} className={'absolute origin-[110px_15px] h-[530px] w-[600px] top-[60px] left-[43%] z-10'}
+                     onMouseMove={(e) => e.stopPropagation()}>
+                    <div className={'rounded-[4px] flex relative shadow-[0_0_40px_5px_rgba(0,0,0,.5)] h-full w-full'}>
+                        <div className={'flex flex-col basis-[220px] pt-[10px] pl-[10px] bg-white relative rounded-tl-[4px] rounded-bl-[4px]'}>
+                            <ContentMenuItem name={'AlgoExpert'} onMouseOver={() => setItemSelected('algoexpert')} selected={itemSelected === 'algoexpert'} />
+                            <ContentMenuItem name={'SystemsExpert'} onMouseOver={() => setItemSelected('systemsexpert')} />
+                            <ContentMenuItem name={'MlExpert'} onMouseOver={() => setItemSelected('mlexpert')}/>
+                            <ContentMenuItem name={'InfraExpert'} onMouseOver={() => setItemSelected('infraexpert')} />
+                            <ContentMenuItem name={'BlockChain'} onMouseOver={() => setItemSelected('blockchainexpert')} />
+                            <ContentMenuItem name={'Bonus'} onMouseOver={() => setItemSelected('bonus')} />
+                        </div>
+                        { getSelectedItem(itemSelected) }
+                    </div>
+                </motion.div>
+            ) : null }
         </AnimatePresence>
-    )
+    );
 }
 
 
-const AlgoExpertSubMenu = () => {
+const ContentMenuItem = ({ name, onMouseOver, selected, bgColor }) => {
 
-    return <section className={'p-[15px] bg-[#626ee3] flex flex-col'}>
-        <Link href={'/'} className={'flex gap-[20px] rounded-[4px] max-h-[60px] px-[15px] text-white bg-[rgba(0,0,0,.3)]'}>
+   return (
+       <div onMouseOver={onMouseOver}
+            style={{ background: selected ? '#626ee3' : '#fff', color: selected ? '#fff' : '#02203c' }}
+            className={'flex justify-between cursor-pointer p-[15px] w-full font-bold text-[16px] bg-white text-[#02203c] relative rounded-tl-[4px] rounded-bl-[4px] hover:bg-[#626ee3] hover:text-white header__content_menu--hover'}>
+           <span>{ name }</span>
+           <ArrowIcon className={'w-[10px] rotate-[-90deg]'} />
+       </div>
+   );
+}
+
+
+const AlgoExpertMenu = () => {
+    return (
+        <section className={'flex flex-col p-[15px] flex-1 bg-[#626ee3] h-full text-white gap-[15px] rounded-[4px]'}>
+            <ContentMenuLink href={'/'} name={'Coding Interview Questions'} />
+            <ContentMenuLink href={'/'} name={'Data Structures Crash Course'} />
+            <ContentMenuLink href={'/'} name={'Coding Interview Assessments'} />
+            <ContentMenuLink href={'/'} name={'Mock Coding Interviews'} />
+            <ContentMenuLink href={'/'} name={'Coding Interview Tips'} />
+        </section>
+    );
+}
+
+const SystemExpertMenu = () => {}
+const MlExpertMenu = () => {}
+const InfraExpertMenu = () => {}
+const BlockChainExpertMenu = () => {}
+const BonusMenu = () => {}
+
+const ContentMenuLink = ({ href, name, Icon }) => {
+    return (
+        <Link href={href} className={'flex gap-[20px] rounded-[4px] max-h-[60px] px-[15px] bg-[rgba(0,0,0,.3)] hover:bg-[rgba(0,0,0,.5)] py-[15px]'}>
             <InterviewQuestionsIcon className={'w-[30px]'} />
-            Coding Interview Questions
+            { name }
         </Link>
-    </section>
+    );
 }
 
 export default ContentMenu
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,12 @@
 'use client'
 
-import React, {useState} from 'react'
+import React from 'react'
 import SignUpModal from "@/components/SignUpModal.jsx";
 import ProductMenu from "@/components/ProductMenu.jsx";
 import HeaderLogo from "@/components/HeaderLogo.jsx";
 import LogInButton from "@/components/LogInButton.jsx";
 import ContentMenu from "@/components/ContentMenu";
-import ArrowDownIcon from '../icons/ArrowDown.svg';
-import {signOut, useSession} from "next-auth/react";
-import Image from "next/image";
-import {AnimatePresence, motion} from "framer-motion";
-import ProfileIcon from '../icons/ProfileIcon.svg';
-import AccountIcon from '../icons/Account.svg';
-import DoorIcon from '../icons/DoorIcon.svg';
+import {useSession} from "next-auth/react";
 import ProfileMenu from "@/components/ProfileMenu";
 
 
@@ -24,6 +18,8 @@ const Header = ({ children }) => {
 
     const [profileMenuOpen, setProfileMenuOpen] = React.useState(false);
 
+    const [isContentMenuOpen, setIsContentMenuOpen] = React.useState(false);
+
     const { data } = useSession();
 
     const isLoggedIn = data?.user;
@@ -31,6 +27,10 @@ const Header = ({ children }) => {
     const handleOnMouseOver = (e) => {
         e.stopPropagation()
         setIsProductMenuOpen(true)
+    }
+
+    const handleOnMouseOverContentMenu = () => {
+        setIsContentMenuOpen(true);
     }
 
     const handleClickOutside = () => {
@@ -41,10 +41,15 @@ const Header = ({ children }) => {
         setIsModalOpen(true)
     }
 
+    const handleOnMouseMoveHeader = () => {
+        setIsProductMenuOpen(false);
+        setIsContentMenuOpen(false);
+    }
+
     return (
             <header
                 className={'h-[540px] bg-gradient-to-r from-[#02203c] to-[#001528] flex flex-col'}
-                onMouseMove={() => setIsProductMenuOpen(false)}
+                onMouseMove={handleOnMouseMoveHeader}
                 onClick={handleClickOutside}
             >
                 <div className={'mx-[20px] py-[30px] flex justify-center items-center relative'}>
@@ -54,7 +59,7 @@ const Header = ({ children }) => {
                         className={'flex relative mx-[55px] gap-[30px] h-full items-start font-open_sans text-[14px] text-white'}
                         onMouseMove={(e) => e.stopPropagation()}>
                         <div onMouseOver={handleOnMouseOver} className={'bg-transparent relative nav__link'}>Products</div>
-                        <div className={'bg-transparent relative nav__link'}>Content</div>
+                        <div onMouseOver={handleOnMouseOverContentMenu} className={'bg-transparent relative nav__link'}>Content</div>
                         <div className={'bg-transparent relative nav__link'}>Team</div>
                         <div className={'bg-transparent font-bold relative italic nav__link'}>Purchase</div>
                     </nav>
@@ -71,7 +76,7 @@ const Header = ({ children }) => {
                 </div>
                 <SignUpModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}/>
                 <ProductMenu isOpen={isProductMenuOpen} onClose={() => setIsProductMenuOpen(false)}/>
-                <ContentMenu />
+                <ContentMenu isOpen={isContentMenuOpen} />
                 { children }
             </header>
     )
