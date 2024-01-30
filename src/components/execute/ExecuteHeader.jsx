80e5efafc89b970d1a36bbf8575196b5899529e7
Bekjon Ibragimov <bekjon_98@mail.ru>
2024-01-30 17:15:21 +0200

    Timer and Language Dropdown added

--- src/components/execute/ExecuteHeader.jsx
+++ src/components/execute/ExecuteHeader.jsx
@@ -6,13 +6,17 @@ import BugIcon from "@/icons/BugIcon.svg";
 import SettingIcon from "@/icons/SettingIcon.svg";
 import ProgrammingLanguageDropdown from "@/components/execute/ProgrammingLanguageDropdown";
 import Timer from "@/components/execute/Timer";
+import AlgoExpertIcon from "@/icons/HeaderIcon.svg";
 
 const ExecuteHeader = () => {
 
     return <div className={"flex justify-between rounded-[4px] mb-[15px]"}>
         <div className={"flex gap-[15px]"}>
-            <Link href={"/"}
-                  className={"rounded-[4px] transition-colors bg-[#626ee3] text-white p-[10px]"}>AlgoExpert</Link>
+            <Link href={"/product"}
+                  className={"rounded-[4px] justify-center flex gap-[.5rem] items-center max-w-[150px] w-full transition-colors bg-[#626ee3] text-white px-[10px] py-[7px]"}>
+                    <AlgoExpertIcon className={'w-[25px] h-[27px]'} />
+                    AlgoExpert
+            </Link>
             <Link href={"/questions"}
                   className={"bg-[#15314b] transition-colors text-white rounded-[4px] flex justify-center items-center w-[55px] h-[40px] hover:bg-[#626ee3]"}>
                 <ListIcon className={"max-w-[25px] max-h-[25px]"}/>
--- src/components/execute/Timer.jsx
+++ src/components/execute/Timer.jsx
@@ -1,3 +1,14 @@
-const Timer = () => { }
+import TimerIcon from '@/icons/ClockIcon.svg';
+
+const Timer = () => {
+    return <div>
+        <button className={'rounded-bl-[4px] rounded-br-[4px] bg-[#15314b] font-bold px-[15px] h-[40px] hover:bg-[#626ee3]'}>
+            <span className={'flex justify-center items-center whitespace-nowrap text-white'}>
+                <TimerIcon className={'w-[25px]'} />
+                <div className={'ml-[10px] font-open_sans text-[14px]'}>00:00:00</div>
+            </span>
+        </button>
+    </div>
+}
 
 export default Timer;
