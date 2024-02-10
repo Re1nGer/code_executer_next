f195374fe9884885934775e5ec4399f2e402c170
Bekjon Ibragimov <bekjon_98@mail.ru>
2024-02-10 12:12:19 +0200

    update

--- src/components/productQuestionLanding/ProductQuestionSection.jsx
+++ src/components/productQuestionLanding/ProductQuestionSection.jsx
@@ -1,8 +1,40 @@
 import Link from "next/link";
-import TickIcon from '@/icons/TickIcon.svg';
+import { useInView, useAnimate, motion } from "framer-motion";
+import { useRef } from "react";
+import ProductQuestionCard from "@/components/productQuestionLanding/ProductQuestionCard";
 
+const list = {
+    visible: {
+        opacity: 1,
+        transition: {
+            delay: 1,
+            when: "beforeChildren",
+            staggerChildren: 0.07,
+        },
+    },
+    hidden: { opacity: 1 },
+}
+
+const item = {
+    visible: { z: 0, transition: { ease: "easeInOut" } },
+    hidden: {  z: 200 },
+}
 
 const ProductQuestionSection = () => {
+
+    const questionCardContainer = useRef(null);
+
+    const isInView = useInView(questionCardContainer, { once: true });
+
+    const [scope, animate] = useAnimate();
+
+/*
+    useEffect(() => {
+        if (isInView) {
+            animate('ul', { opacity: 1 })
+        }
+    }, [isInView]);
+*/
 
     return <section className={'bg-[#02203c] py-[100px] px-[20px]'}>
         <h2 className={'text-white mb-[20px] text-[35px] text-center'}>200 Questions Spanning 15 Categories</h2>
@@ -15,42 +47,28 @@ const ProductQuestionSection = () => {
                   className={'bg-[#626ee3] h-full font-open_sans shadow-[0_2px_5px_0_rgba(0,0,0,.5)] rounded-[4px] px-[40px] text-white flex justify-center items-center whitespace-nowrap'}>
                 Explore Questions</Link>
         </div>
-        <div className={'mt-[40px]'} style={{ perspective: '1000px' }}>
-            <ul className={'max-w-[1000px] mx-auto text-white flex gap-[40px] flex-wrap'}>
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard title={'Arrays'} backText={'Harder than they seem'} />
-            </ul>
-        </div>
+        <motion.div className={'mt-[40px]'} variants={list} initial={'hidden'} animate={'visible'} ref={scope}>
+            <motion.ul className={'max-w-[1000px] mx-auto text-white flex gap-[40px] flex-wrap'} style={{ perspective: '1000px' }} ref={questionCardContainer}>
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+            </motion.ul>
+        </motion.div>
     </section>;
 }
 
 
 export default ProductQuestionSection;
 
-
-const ProductQuestionCard = ({ title, backText }) => {
-    return <li className={'max-w-[306px] w-full'}>
-        <div className={'shadow-[2px_2px_15px_0_rgba(0,0,0,.4)] w-full relative rounded-[4px] product__question-section_landing-card_container product__question-section_landing-card_list'}>
-            <div className={'flex flex-col bg-[#15314b] w-full justify-center items-center p-[30px] text-center product__question-section_landing-card_inner'}>
-                <div className={'bg-[#e4e4e4] h-[35px] w-[35px] mb-[15px] rounded-full flex justify-center items-center'}>
-                    <TickIcon className={'text-[#02203c] w-[20px]'} />
-                </div>
-                {title}
-            </div>
-            <div className={'absolute left-0 top-0 w-full h-full product__question-section_landing-card_back'}>{backText}</div>
-        </div>
-    </li>
-}
