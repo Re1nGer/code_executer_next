975c0212936b14a1fb15e3f51036ddf77affe09e
Tomas <bekjon_98@mail.ru>
2024-03-26 18:13:43 +0200

    update

--- src/components/ProductLanding.jsx
+++ src/components/ProductLanding.jsx
@@ -2,10 +2,11 @@ import { motion } from "framer-motion";
 import BuyAlgoExpertButton from "@/components/BuyAlgoExpertButton.jsx";
 
 const ProductLanding = () => {
-    return <div className={"max-w-[500px] flex flex-col items-start justify-center mx-auto"}>
+    return <div className={"max-w-[500px] flex flex-col items-start justify-center mx-auto px-[20px] md:px-0"}>
         <h1 className={"text-[38px] mb-[20px] text-center md:text-left w-full font-ubuntu"}>AlgoExpert</h1>
-        <p className={"font-open_sans text-[18px] mb-[30px] text-center md:text-left"}>The ultimate resource to prepare for coding
+        <p className={"font-open_sans text-[18px] mb-[30px] text-center md:text-left leading-[1.7]"}>The ultimate resource to prepare for coding
             interviews.
+            <br />
             Everything you need, in one streamlined platform.</p>
         <motion.div initial={{scale: .5}} animate={{scale: 1, opacity: 1}} className={"w-full opacity-0 flex justify-center md:justify-start"}>
             <BuyAlgoExpertButton className={"w-full"}/>
--- src/components/productQuestionLanding/ProductQuestionSection.jsx
+++ src/components/productQuestionLanding/ProductQuestionSection.jsx
@@ -1,6 +1,6 @@
+import React from 'react';
 import Link from "next/link";
-import { useAnimate, motion } from "framer-motion";
-import { useRef } from "react";
+import {motion, stagger, useAnimate, useInView} from "framer-motion";
 import ProductQuestionCard from "@/components/productQuestionLanding/ProductQuestionCard";
 
 const list = {
@@ -16,15 +16,24 @@ const list = {
 }
 
 const item = {
-    visible: { z: 0, transition: { ease: "easeInOut" } },
-    hidden: {  z: 200 },
+    visible: { z: 0, transition: { ease: "easeInOut" }, opacity: 1 },
+    hidden: {  z: 200, opacity: 0 },
 }
 
 const ProductQuestionSection = () => {
 
-    const questionCardContainer = useRef(null);
-
     const [scope, animate] = useAnimate();
+
+    const inView = useInView(scope);
+
+
+    React.useEffect(() => {
+        if (inView) {
+            animate('li', { z: 0, opacity: 1 },  { delay: stagger(0.07) })
+        }
+    }, [inView])
+
+    console.log(inView);
 
     return <section className={'bg-[#02203c] py-[100px] px-[20px]'}>
         <h2 className={'text-white mb-[20px] text-[30px] md:text-[35px] text-center font-ubuntu'}>200 Questions Spanning 15 Categories</h2>
@@ -37,23 +46,28 @@ const ProductQuestionSection = () => {
                   className={'bg-[#626ee3] h-full font-open_sans shadow-[0_2px_5px_0_rgba(0,0,0,.5)] rounded-[4px] px-[40px] text-white flex justify-center items-center whitespace-nowrap'}>
                 Explore Questions</Link>
         </div>
-        <motion.div className={'mt-[40px]'} variants={list} initial={'hidden'} animate={'visible'} ref={scope}>
-            <motion.ul className={'max-w-[1000px] mx-auto text-white flex gap-[20px] md:gap-[40px] flex-wrap justify-center lg:justify-start'} style={{ perspective: '1000px' }} ref={questionCardContainer}>
-                <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
-                <ProductQuestionCard variants={item} title={'Graphs'} backText={'Easier than they seem'} />
-                <ProductQuestionCard variants={item} title={'Tries'} backText={'Yes. Tries. Not a typo'} />
-                <ProductQuestionCard variants={item} title={'Linked Lists'} backText={'Reverse them'} />
-                <ProductQuestionCard variants={item} title={'Binary Trees'} backText={'Invert them.'} />
-                <ProductQuestionCard variants={item} title={'Strings'} backText={'"This is a string"'} />
-                <ProductQuestionCard variants={item} title={'Dynamic Programming'} backText={'Everyone hates it.'} />
-                <ProductQuestionCard variants={item} title={'Searching'} backText={'Searching for the purchase button?'} />
-                <ProductQuestionCard variants={item} title={'Sorting'} backText={'array = [3,1,2] sort(array) array = [1,2,3]'} />
-                <ProductQuestionCard variants={item} title={'Greedy Algorithms'} backText={'Not too greedy'} />
-                <ProductQuestionCard variants={item} title={'Recursion'} backText={'[[[[[[[[[[]]]]]]]]]]'} />
-                <ProductQuestionCard variants={item} title={'Famous Algorithms'} backText={'ヽ( ͡° ͜ʖ ͡°)ﾉ'} />
-                <ProductQuestionCard variants={item} title={'Heaps'} backText={'Fancy trees.'} />
-                <ProductQuestionCard variants={item} title={'Binary Search Trees'} backText={'BS Trees'} />
-                <ProductQuestionCard variants={item} title={'Stacks'} backText={'Push. Pop. That\'s about it '} />
+        <motion.div className={'mt-[40px]'}>
+            <motion.ul
+               initial={'hidden'}
+               variants={list}
+               className={'max-w-[1000px] mx-auto text-white flex gap-[20px] md:gap-[40px] flex-wrap justify-center lg:justify-start'}
+               style={{ perspective: '1000px' }}
+               ref={scope}>
+                    <ProductQuestionCard variants={item} title={'Arrays'} backText={'Harder than they seem'} />
+                    <ProductQuestionCard variants={item} title={'Graphs'} backText={'Easier than they seem'} />
+                    <ProductQuestionCard variants={item} title={'Tries'} backText={'Yes. Tries. Not a typo'} />
+                    <ProductQuestionCard variants={item} title={'Linked Lists'} backText={'Reverse them'} />
+                    <ProductQuestionCard variants={item} title={'Binary Trees'} backText={'Invert them.'} />
+                    <ProductQuestionCard variants={item} title={'Strings'} backText={'"This is a string"'} />
+                    <ProductQuestionCard variants={item} title={'Dynamic Programming'} backText={'Everyone hates it.'} />
+                    <ProductQuestionCard variants={item} title={'Searching'} backText={'Searching for the purchase button?'} />
+                    <ProductQuestionCard variants={item} title={'Sorting'} backText={'array = [3,1,2] sort(array) array = [1,2,3]'} />
+                    <ProductQuestionCard variants={item} title={'Greedy Algorithms'} backText={'Not too greedy'} />
+                    <ProductQuestionCard variants={item} title={'Recursion'} backText={'[[[[[[[[[[]]]]]]]]]]'} />
+                    <ProductQuestionCard variants={item} title={'Famous Algorithms'} backText={'ヽ( ͡° ͜ʖ ͡°)ﾉ'} />
+                    <ProductQuestionCard variants={item} title={'Heaps'} backText={'Fancy trees.'} />
+                    <ProductQuestionCard variants={item} title={'Binary Search Trees'} backText={'BS Trees'} />
+                    <ProductQuestionCard variants={item} title={'Stacks'} backText={'Push. Pop. That\'s about it '} />
             </motion.ul>
         </motion.div>
     </section>;
