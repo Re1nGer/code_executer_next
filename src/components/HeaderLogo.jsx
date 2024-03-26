db9e2e3cc27e7f59ca84797b02d4a1bb90977b44
Tomas <bekjon_98@mail.ru>
2024-03-26 18:33:12 +0200

    update

--- src/components/HeaderLogo.jsx
+++ src/components/HeaderLogo.jsx
@@ -18,8 +18,8 @@ const HeaderLogo = () => {
         animate("#header_logo", { rotate: '135deg' }, { duration: 3, type: 'inertia', velocity: 500 } )
     }, []);
 
-    return <Link href={"/product"}>
-        <div className={"w-[290px] flex flex-col gap-[5px] lg:gap-[5px] text-center"}>
+    return <Link href={"/product"} className={'max-w-[290px] flex flex-col gap-[5px] lg:gap-[5px] text-center'}>
+        <div className={""}>
             <div className={"flex items-center justify-center text-white gap-[5px] lg:gap-[15px]"}>
                 <HeaderIcon id={"header_logo"} className={"max-w-[48px] max-h-[20px] sm:max-h-[40px]"}
                             onMouseEnter={handleOnLogoHover}/>
